f770abdf9890c7cde05397d5ba3c29282c9b8854
Yeonggyun Kim <kygha7205@naver.com>
2019-12-25 21:51:41 +0900

    Fix subway query error

--- lib/subway_query.dart
+++ lib/subway_query.dart
@@ -20,7 +20,7 @@ getSubwayData(target) async {
 
   var key = await getTopisKey();
   Map<String, dynamic> parsedData = {
-    "code": "",
+    "code": "INFO-200",
     "dest": "", // 하행선 종착역
     "trainLineNm": "", //하행선 열차종류("오이도행 - 중앙방면")
     "current": "", //하행선 현위치
@@ -32,7 +32,7 @@ getSubwayData(target) async {
     var url = 'http://swopenapi.seoul.go.kr/api/subway/' +
         key.toString() +
         '/json/realtimeStationArrival/0/5/%ED%95%9C%EB%8C%80%EC%95%9E';
-//    print(url);
+    print(url);
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
@@ -43,6 +43,16 @@ getSubwayData(target) async {
       if (subwayList['status'] == 500 && subwayList['code'] == "INFO-200") {
         //운행종료
         parsedData['code'] = subwayList['code'];
+        return parsedData;
+      }
+
+      int idxnum = 0;
+
+      if (subwayList['errorMessage']['total'] == 2) {
+        //데이터 길이가 2일경우(막차 시간대)
+        idxnum = 1;
+      } else if (subwayList['errorMessage']['total'] == 1) {
+        //데이터 길이가 1일경우(막차 시간대)-parsedData 바로 리턴 : 기본값이 INFO-200 운행종료.
         return parsedData;
       }
 
@@ -61,31 +71,37 @@ getSubwayData(target) async {
               ['trainLineNm']; //오이도행 - 상록수방면 : 둘 중에 하나 골라서 사용.
           parsedData["current"] =
               subwayList['realtimeArrivalList'][0]['arvlMsg3']; //상록수
-          if(subwayList['realtimeArrivalList'][0]['arvlMsg3'] == "한대앞" || subwayList['realtimeArrivalList'][0]['arvlMsg3'] == "중앙"){
+          if (subwayList['realtimeArrivalList'][0]['arvlMsg3'] == "한대앞" ||
+              subwayList['realtimeArrivalList'][0]['arvlMsg3'] == "중앙") {
             //전역도착, 한대앞 도착, 한대앞 진입, 전역 진입 의 경우에는 그냥 출력
             parsedData["arvlMsg2"] =
-            subwayList['realtimeArrivalList'][0]['arvlMsg2']; //[1]전역 상록수
+                subwayList['realtimeArrivalList'][0]['arvlMsg2']; //[1]전역 상록수
           } else {
             //그밖의 경우에는 [3]번째 전역 (초지)형식을 다듬어서 리턴.
-            parsedData["arvlMsg2"] = parseString(subwayList['realtimeArrivalList'][0]['arvlMsg2'], subwayList['realtimeArrivalList'][0]['arvlMsg3']);
+            parsedData["arvlMsg2"] = parseString(
+                subwayList['realtimeArrivalList'][0]['arvlMsg2'],
+                subwayList['realtimeArrivalList'][0]['arvlMsg3']);
           }
         }
 
         if (target == "subway_4_lower") {
           //4호선 하행
           parsedData["dest"] =
-              subwayList['realtimeArrivalList'][2]['bstatnNm']; //오이도행
-          parsedData["trainLineNm"] = subwayList['realtimeArrivalList'][2]
+              subwayList['realtimeArrivalList'][idxnum]['bstatnNm']; //오이도행
+          parsedData["trainLineNm"] = subwayList['realtimeArrivalList'][idxnum]
               ['trainLineNm']; //오이도행 - 상록수방면 : 둘 중에 하나 골라서 사용.
           parsedData["current"] =
-              subwayList['realtimeArrivalList'][2]['arvlMsg3']; //상록수
-          if(subwayList['realtimeArrivalList'][2]['arvlMsg3'] == "한대앞" || subwayList['realtimeArrivalList'][2]['arvlMsg3'] == "상록수"){
+              subwayList['realtimeArrivalList'][idxnum]['arvlMsg3']; //상록수
+          if (subwayList['realtimeArrivalList'][idxnum]['arvlMsg3'] == "한대앞" ||
+              subwayList['realtimeArrivalList'][idxnum]['arvlMsg3'] == "상록수") {
             //전역도착, 한대앞 도착, 한대앞 진입, 전역 진입 의 경우에는 그냥 출력
-            parsedData["arvlMsg2"] =
-            subwayList['realtimeArrivalList'][2]['arvlMsg2']; //[1]전역 상록수
+            parsedData["arvlMsg2"] = subwayList['realtimeArrivalList'][idxnum]
+                ['arvlMsg2']; //[1]전역 상록수
           } else {
             //그밖의 경우에는 [3]번째 전역 (초지)형식을 다듬어서 리턴.
-            parsedData["arvlMsg2"] = parseString(subwayList['realtimeArrivalList'][2]['arvlMsg2'], subwayList['realtimeArrivalList'][2]['arvlMsg3']);
+            parsedData["arvlMsg2"] = parseString(
+                subwayList['realtimeArrivalList'][idxnum]['arvlMsg2'],
+                subwayList['realtimeArrivalList'][idxnum]['arvlMsg3']);
           }
         }
       } else {
@@ -100,7 +116,8 @@ getSubwayData(target) async {
   }
 }
 
-parseString(arvlMsg2, arvlMsg3) {//arrvlMsg2 = [3]번째 전역 (초지), arvlMsg2 = 초지 형식.
+parseString(arvlMsg2, arvlMsg3) {
+  //arrvlMsg2 = [3]번째 전역 (초지), arvlMsg2 = 초지 형식.
   final String beforeN = arvlMsg2[1];
 
   final String modStr = beforeN + "역전 : " + arvlMsg3;
